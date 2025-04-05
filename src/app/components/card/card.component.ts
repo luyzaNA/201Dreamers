f5509084fc0b5bb6a3e6c0974909545a6cd6c3db
Magda Paicu <paicumagda962@gmail.com>
2025-04-05 13:58:13 +0300

    added video

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {NgIf} from "@angular/common";
 
 @Component({
@@ -10,12 +10,37 @@ import {NgIf} from "@angular/common";
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
+
 export class CardComponent {
-
   isMusicStarted: boolean = false;
+  @ViewChild('videoElement') videoElement: ElementRef | undefined;
+  audio: HTMLAudioElement | undefined;
 
-  playMusic(){
+  constructor() {
+    this.audio = new Audio('assets/audio/music.mp3');
+  }
+
+  toggleMusic() {
+
+    if (this.isMusicStarted) {
+      this.audio?.pause();
+    } else {
+      this.audio?.play();
+    }
     this.isMusicStarted = !this.isMusicStarted;
   }
 
+  onMouseEnter() {
+    const video = this.videoElement?.nativeElement;
+    if (video) {
+      video.play();
+    }
+  }
+
+  onMouseLeave() {
+    const video = this.videoElement?.nativeElement;
+    if (video) {
+      video.pause();
+    }
+  }
 }
