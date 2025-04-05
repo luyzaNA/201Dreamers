c6b1f5ab3749fef9710ec4bc14d08a2432fe3721
Magda Paicu <paicumagda962@gmail.com>
2025-04-05 16:24:03 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from "@angular/router";
 import { FooterComponent } from "./components/footer/footer.component";
 import {CardComponent} from "./components/card/card.component";
 import {TranslateService} from "@ngx-translate/core";
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-    imports: [RouterOutlet, FooterComponent, CardComponent],
+    imports: [RouterOutlet, FooterComponent, CardComponent, NavBarComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
