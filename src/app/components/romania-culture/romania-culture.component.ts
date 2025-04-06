23e5db5e0718ac8481585b712a99a73c949cdbb2
luyzaNA <146641612+luyzaNA@users.noreply.github.com>
2025-04-06 17:40:57 +0300

    First page

--- src/app/components/romania-culture/romania-culture.component.ts
+++ src/app/components/romania-culture/romania-culture.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-romania-culture',
@@ -8,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrl: './romania-culture.component.css'
 })
 export class RomaniaCultureComponent {
+  private maxZoom: number = 3;
+  highlightShown: boolean = false;
+
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const image = document.querySelector('.zoom-image') as HTMLImageElement | null;
+    const arrow = document.querySelector('.scroll-arrow') as HTMLElement | null;
+    const text = document.querySelector('.material-info') as HTMLElement | null;
+
+    if (image && arrow && text) {
+      const scrollTop = window.scrollY;
+      const scale = 1 + scrollTop / 1000;
+      const clampedScale = Math.min(scale, this.maxZoom);
+
+      image.style.transform = `scale(${clampedScale})`;
+
+      const scrollOffset = scrollTop / 5;
+      arrow.style.transform = `translateY(${scrollOffset}px)`;
+
+      if (clampedScale >= this.maxZoom) {
+        arrow.classList.add('fixed');
+        text.classList.add('fixed');
+
+      } else {
+        arrow.classList.remove('fixed');
+        text.classList.add('fixed');
+
+      }
+      if (scrollTop > 99 && clampedScale < this.maxZoom) {
+        arrow.style.opacity = '1';
+        text.style.opacity = '1';
+      } else {
+        arrow.style.opacity = '0';
+        text.style.opacity = '0';
+      }
+    }
+  }
 
 }
